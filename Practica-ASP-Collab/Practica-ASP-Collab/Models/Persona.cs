43a8ee675ae743c8757fc01b8fe0dd16ee64750b
paulmao <gomezpaulina1509@gmail.com>
2020-02-21 17:04:59 -0400

    Añadiendo la clase persona

---
+++ Practica-ASP-Collab/Practica-ASP-Collab/Models/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica_ASP_Collab.Models
+{
+    public class Persona
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Genero { get; set; }        
+        public Vacante Vac { get; set; }
+        
+
+    }
+
+    public enum Vacante
+    {
+
+        Dessarrollador, Disenador, Manager, Analista    }
+}
