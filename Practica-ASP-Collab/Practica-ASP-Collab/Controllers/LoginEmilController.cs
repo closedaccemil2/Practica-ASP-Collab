23a312aef7d851157383f3d192161576827f2819
emilbarranco <rehser@outlook.com>
2020-02-21 15:11:36 -0400

    Controlador para el Login con Facebook creado.

---
+++ Practica-ASP-Collab/Practica-ASP-Collab/Controllers/LoginEmilController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica_ASP_Collab.Controllers
+{
+    public class LoginEmilController : Controller
+    {
+        // GET: LoginEmil
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
