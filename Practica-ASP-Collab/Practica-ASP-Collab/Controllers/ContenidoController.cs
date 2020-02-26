833a5b303baeec5ae7e51f70416aff708d15d77d
paulmao <gomezpaulina1509@gmail.com>
2020-02-26 10:12:53 -0400

    Filtro de contenidos y About de Paulina

---
+++ Practica-ASP-Collab/Practica-ASP-Collab/Controllers/ContenidoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica_ASP_Collab.Controllers
+{
+    public class ContenidoController : Controller
+    {
+        // GET: Contenido
+        public ActionResult Peliculas()
+        {
+            return View();
+        }
+        public ActionResult Noticias()
+        {
+            return View();
+        }
+        public ActionResult Musica()
+        {
+            return View();
+        }
+
+    }
+}
